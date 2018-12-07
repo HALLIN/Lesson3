bd1b6e854c9ba80f86595205afc21e83d83ffd2b
HALLIN <garnik.nersisyan.y@tumo.org>
2018-12-07 19:14:45 +0400

    Start

---
+++ index.js
@@ -0,0 +1,32 @@
+/*var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+   res.send("<h1>Hello world</h1>");
+});
+app.get("/name/:name", function(req, res){
+   var name = req.params.name;
+   res.send("<h1>Hello " + name +"</h1>");
+});
+app.get("/google/:search", function(req, res){
+    var search = req.params.search;
+    res.redirect('http://google.com/search?q='+ search);
+ });
+ app.get("/google", function(req, res){
+    res.redirect('http://google.com');
+ });
+ app.get("/*", function(req, res){
+    res.send("<h1>Error 404</h1>");
+ });
+app.listen(3000, function(){
+   console.log("Example is running on port 3000");
+});*/
+
+
+var Square = require("./module");
+var mySquareObject = new Square(5);
+
+function main() {
+   console.log(mySquareObject.getArea());
+}
+main();
